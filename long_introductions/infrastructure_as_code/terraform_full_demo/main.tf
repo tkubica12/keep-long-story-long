c4e3c85c150a61927baaf35bd66945712b6155c3
Tomáš Kubica <tkubica12@gmail.com>
2023-08-16 14:17:35 +0000

    final project

---
+++ long_introductions/infrastructure_as_code/terraform_full_demo/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "main" {
+  name     = "tf-demo-rg"
+  location = "West Europe"
+}
+
+module "sql" {
+  source                  = "./modules/azuresql"
+  for_each                = fileset("${path.module}/databases", "*.yaml")
+  sql_server_prefix       = element(split(".", each.key), 0)
+  location                = azurerm_resource_group.main.location
+  resource_group_name     = azurerm_resource_group.main.name
+  key_vault_id            = azurerm_key_vault.main.id
+  sql_database_name       = yamldecode(file("${path.module}/databases/${each.key}")).db_name
+  sql_database_sku        = yamldecode(file("${path.module}/databases/${each.key}")).sku
+  enable_private_endpoint = yamldecode(file("${path.module}/databases/${each.key}")).enable_private_endpoint
+  subnet_id               = azurerm_subnet.main.id
+  private_dns_zone_id     = azurerm_private_dns_zone.sql.id
+}
---
+++ long_introductions/infrastructure_as_code/terraform_full_demo/networking.tf
@@ -0,0 +1,26 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                                          = "default"
+  resource_group_name                           = azurerm_resource_group.main.name
+  virtual_network_name                          = azurerm_virtual_network.main.name
+  address_prefixes                              = ["10.0.1.0/24"]
+  private_link_service_network_policies_enabled = true
+}
+
+resource "azurerm_private_dns_zone" "sql" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql" {
+  name                  = "sql-link"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+}
