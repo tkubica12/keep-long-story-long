f81dc970db19b6363eb9738db73ffb5a2b2a2e36
Tomáš Kubica <tkubica12@gmail.com>
2023-08-17 04:37:43 +0000

    provider

--- long_introductions/infrastructure_as_code/terraform_full_demo/providers.tf
+++ long_introductions/infrastructure_as_code/terraform_full_demo/providers.tf
@@ -15,6 +15,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "longintro.iac.tfstate"
     subscription_id      = "d3b7888f-c26e-4961-a976-ff9d5b31dfd3"
+    use_oidc             = true
   }
 }
 
